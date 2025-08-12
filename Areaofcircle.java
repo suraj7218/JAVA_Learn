95aa41518f872b9fd07a0d40c3dabc2e3940cb99
suraj7218 <surajrangade9822@gmail.com>
2025-08-12 12:14:03 +0530

    12-08

---
+++ Areaofcircle.java
@@ -0,0 +1,12 @@
+class Areaofcircle
+{
+	public static void main(String args[])
+	{
+		int r=2;
+		float pi=13.14f;
+
+		float res=pi*r*r;
+		
+		System.out.println("the area of the circle is :"+res);
+	}
+}

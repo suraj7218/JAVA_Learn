31caed778f304bde4bf3f4d7c776c28f24183fb5
suraj7218 <surajrangade9822@gmail.com>
2025-08-12 11:10:44 +0530

    12-08

---
+++ Voting.java
@@ -0,0 +1,15 @@
+class Voting
+{
+public static void main(String args[])
+{
+	int num=15;
+	if(num>=18)
+	{
+		System.out.println("Eligible for the vote");
+	}
+	else
+	{
+		System.out.println("Not Eligible for the vote");
+	}
+}
+}

c384155978165b464dc328e109e558f2003fb680
suraj7218 <surajrangade9822@gmail.com>
2025-08-14 12:52:13 +0530

    14-08

---
+++ Whileloop.java
@@ -0,0 +1,11 @@
+public class Whileloop {
+    public static void main(String args[]){
+        int coutdown = 4;
+        while (coutdown>0) {
+            System.out.println(coutdown);
+            coutdown--;
+        }
+        System.out.println("Hello World......");
+    }
+    
+}

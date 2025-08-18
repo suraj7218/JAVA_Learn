7abeddd6bd040e53a982a0cb7fcb43a97078610f
suraj7218 <surajrangade9822@gmail.com>
2025-08-18 12:40:26 +0530

    18-25

--- Whileloop.java
+++ Whileloop.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Whileloop {
     public static void main(String args[]){
-        int coutdown = 4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for the countdown..:");
+        int coutdown = sc.nextInt();
         while (coutdown>0) {
             System.out.println(coutdown);
             coutdown--;

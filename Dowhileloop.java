c866ae861c1aca67a810e699642b0042c355f924
suraj7218 <surajrangade9822@gmail.com>
2025-08-19 15:13:20 +0530

    19-08

---
+++ Dowhileloop.java
@@ -0,0 +1,11 @@
+public class Dowhileloop {
+    public static void main(String args[])
+        {
+            int i =1;
+            do
+            {
+            System.out.println("number is "+ i);
+            i++;
+            }while(i<5);
+        }
+}
